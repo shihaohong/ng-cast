1c37e8d854d3f26c307201f47c756cac335e02eb
Hack Reactor Student <student@hackreactor.com>
2017-06-06 16:44:44 -0700

    Implement search click tied with proper YouTube $http request

--- src/components/search.js
+++ src/components/search.js
@@ -3,7 +3,10 @@ angular.module('video-player')
 .component('search', {
   
   controller: function(youTube) {
-    console.log(youTube);
+    this.search = function(input) {
+      youTube.search(input);
+      console.log('search');
+    };
   },
 
   templateUrl: 'src/templates/search.html'
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -2,21 +2,23 @@ angular.module('video-player')
 .service('youTube', function($http, $window) {
   var apiKey = $window.YOUTUBE_API_KEY;
 
-  $http({
-    method: 'GET',
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    params: {
-      maxResults: 5,
-      q: 'dogs',
-      part: 'snippet',
-      key: apiKey
-    }
-  }).then(function successCallback(response) {
-    console.log(response.data.items);
+  this.search = function(query, callback) { 
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        maxResults: 5,
+        q: query,
+        part: 'snippet',
+        key: apiKey
+      }
+    }).then(function successCallback(response) {
+      console.log(response.data.items);
 
 
-  }, function errorCallback(response) {
-    console.log('Error: GET request failed');
+    }, function errorCallback(response) {
+      console.log('Error: GET request failed');
 
-  });
+    });
+  };
 });
